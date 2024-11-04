7abaf5cc8dd820fe8c7371bcd3eeea7611433e00
XiaomA <805122541@qq.com>
2024-11-04 17:10:46 +0800

    2.1 函数编程：购物车案例——初始化数据

---
+++ src/main/java/com/xiaoweii/cart/SkuCategoryEnum.java
@@ -0,0 +1,30 @@
+package com.xiaoweii.cart;
+
+/**
+ * 商品类型枚举
+ */
+public enum SkuCategoryEnum {
+    CLOTHING(10,"服装类"),
+    ELECTRONICS(20,"数码类"),
+    SPORTS(30,"运动类"),
+    BOOKS(40,"图书类");
+
+    /**
+     * code 商品类型编号
+     */
+    private Integer code;
+    /**
+     * name 商品类型名称
+     */
+    private String name;
+
+    /**
+     * 构造方法
+     * @param code
+     * @param name
+     */
+    SkuCategoryEnum(Integer code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+}
